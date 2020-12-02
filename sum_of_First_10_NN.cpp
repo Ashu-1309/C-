709c3b8371173ab1cedfcc1cbb4337bd52c2aba2
Ashutosh Kumar Saw <60690692+Ashu-1309@users.noreply.github.com>
2020-12-02 19:17:29 +0530

    Sum of natural Number
    It is basically a simple code that will give the sum of first 10 natural number.

---
+++ sum_of_First_10_NN.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,sum=0;
+    cout <<"Sum of the First 10 Natural  Number"<<endl;
+    for(i=1; i<=10; i++)
+    {
+       sum=sum+i;
+
+    }
+    cout <<sum << " ";
+}
